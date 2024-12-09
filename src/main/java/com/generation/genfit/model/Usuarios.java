b7e6b3d38a41415593e66450ae960fb1f786fa2a
AlvessGS <gabriels.alvess25@gmail.com>
2024-12-09 11:51:35 -0300

    feat: model usuario

---
+++ src/main/java/com/generation/genfit/model/Usuarios.java
@@ -0,0 +1,63 @@
+package com.generation.genfit.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table (name = "tb_usuarios")
+public class Usuarios {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(min = 8, message = "A Senha deve ter no mínimo 8 caracteres")
+	private String senha; 
+	
+	@NotBlank
+	@Email (message = "O atributo email é obrigatorio!")
+	private String usuario;
+	
+	@Size (max = 5000)
+	private String foto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+}
